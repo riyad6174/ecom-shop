222c01aff4ddc24a56796afc8edd5c477ea3a754
Riyad <alimranhosen21@gmail.com>
2025-08-18 00:12:31 +0600

    color remove

--- src/utils/products.js
+++ src/utils/products.js
@@ -43,7 +43,7 @@ export const products = [
       { color: 'Gray' },
       { color: 'Pink' },
       { color: 'cornflowerblue' },
-      { color: 'Beige' },
+      // { color: 'Beige' },
     ],
   },
   {
