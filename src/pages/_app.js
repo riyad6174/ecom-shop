b7bafc0615332e317daa6a792d3bce96161ba6ac
Riyad <alimranhosen21@gmail.com>
2026-02-16 00:27:11 +0600

    update gtm

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,12 @@
 import { store } from '@/store';
 import '@/styles/globals.css';
 import { Provider } from 'react-redux';
+import { GoogleTagManager } from '@next/third-parties/google';
 
 export default function App({ Component, pageProps }) {
   return (
     <Provider store={store}>
+      <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID || 'GTM-MJXWJ24V'} />
       <Component {...pageProps} />
     </Provider>
   );
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -23,21 +23,7 @@ export default function Document() {
           href='https://fonts.googleapis.com/css2?family=Noto+Serif+Bengali:wght@100..900&display=swap'
           rel='stylesheet'
         ></link>
-        {/* Google Tag Manager */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','${
-                process.env.NEXT_PUBLIC_GTM_ID || 'GTM-MJXWJ24V'
-              }');
-            `,
-          }}
-        />
-        {/* End Google Tag Manager */}
+
         {/* Meta Pixel Code */}
         {/* <script
           dangerouslySetInnerHTML={{
@@ -60,19 +46,7 @@ export default function Document() {
         {/* End Meta Pixel Code */}
       </Head>
       <body className='antialiased'>
-        {/* Google Tag Manager (noscript) */}
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `
-              <iframe https://www.googletagmanager.com/ns.html?id=${
-                process.env.NEXT_PUBLIC_GTM_ID || 'GTM-MJXWJ24V'
-              }
-              height="0" width="0" style="display:none;visibility:hidden"></iframe>
-            `,
-          }}
-        />
 
-        {/* End Google Tag Manager (noscript) */}
         {/* Meta Pixel (noscript) */}
         {/* <noscript>
           <img
